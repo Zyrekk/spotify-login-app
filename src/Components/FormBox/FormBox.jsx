c2e48e98b6fc2a6f39ccb10868c0b28ab5e5f6e0
Zyrekk <zyrakonrad09@gmail.com>
2023-04-24 19:22:55 +0200

    Final version view

--- src/Components/FormBox/FormBox.jsx
+++ src/Components/FormBox/FormBox.jsx
@@ -1,9 +1,16 @@
 import styles from "./FormBox.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpotify } from '@fortawesome/free-brands-svg-icons'
+import {useState} from "react";
 
 
 const FormBox=()=>{
+
+    const [signType,setSignType]=useState('login')
+
+    let signInClass=signType==='login'?styles.checked:''
+    let signUpClass=signType==='login'?'':styles.checked
+
     return(
         <div className={styles.Container}>
             <div className={styles.LogoContainer}>
@@ -11,24 +18,45 @@ const FormBox=()=>{
                 <span>Spotify</span>
             </div>
             <div className={styles.ButtonsContainer}>
-                <button>SIGN IN</button>
-                <button>SIGN UP</button>
+                <button className={`${styles.InButton} ${signInClass}`} onClick={()=>{
+                    setSignType('login')
+                }}>SIGN IN</button>
+                <button className={`${styles.UpButton} ${signUpClass}`} onClick={()=>{
+                    setSignType('register')
+                }}>SIGN UP</button>
             </div>
-            <div className={styles.FormContainer}>
+            <form className={signType==='login'?styles.LoginContainer:styles.LoginContainerHidden}>
                 <div className={styles.FormBox}>
                     <input type="text" placeholder="Username"/>
-                    <input type="text" placeholder="Password"/>
+                    <input type="password" placeholder="Password"/>
                     <div className={styles.CheckBox}>
                         <label>
-                            <input type='checkbox'/>
+                            <input type='checkbox' onClick={()=>{
+                                console.log("CLICK")}}/>
                             <span className={styles.InputCheck}/>
-                            stay signed in
+                            Stay signed in
                         </label>
                     </div>
-                    <button>SIGN IN</button>
-                    <button>Forgot Password?</button>
+                    <button type="submit" className={styles.SignInButton}>SIGN IN</button>
+                    <button className={styles.Forgot}>Forgot Password?</button>
                 </div>
-            </div>
+            </form>
+            <form className={signType!=='login'?styles.RegisterContainer:styles.RegisterContainerHidden}>
+                <div className={styles.FormBox}>
+                    <input type="text" placeholder="Username"/>
+                    <input type="text" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <div className={styles.CheckBox}>
+                        <label>
+                            <input type='checkbox' onClick={()=>{
+                                console.log("CLICK")}}/>
+                            <span className={styles.InputCheck}/>
+                            Accept terms & privacy
+                        </label>
+                    </div>
+                    <button type="submit" className={styles.SignInButton}>SIGN UP</button>
+                </div>
+            </form>
         </div>
     )
 }
